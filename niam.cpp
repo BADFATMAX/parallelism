e5b5e82a9f8a1a86afa55bfb09507d881bab67cf
BADFATMAX <parturnax22@gmail.com>
2022-02-10 04:11:31 +0000

    float test

---
+++ niam.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+#define _USE_MATH_DEFINES
+using namespace std;
+int main() {
+    float s = 0;
+    int n = 10000000;
+    float *a = new float[n];
+    #pragma acc kernels
+    #pragma data acc copyin(a) copyout(s)
+    for (int i = 0; i < n; i++) {
+        a[i] = sin(i * 2 * M_PI / float(n));
+        s += a[i];
+        //cout << (a[i]);
+    }
+//    for (int i = 0; i < n; i++) {
+//        s += a[i];
+//    }
+    cout << s << endl;
+    return 0;
+}
